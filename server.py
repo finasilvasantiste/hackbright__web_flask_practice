365226eca9bbaf4022afbe264d9877e48acab9a2
Hackbright Student <no-reply@hackbrightacademy.com>
2019-01-22 15:21:25 -0800

    Creating dropdown with a loop

--- server.py
+++ server.py
@@ -13,6 +13,8 @@ AWESOMENESS = [
     'oh-so-not-meh', 'brilliant', 'ducky', 'coolio', 'incredible',
     'wonderful', 'smashing', 'lovely']
 
+DISSES = ['dummkopf', 'rumpelstilzchen', 'babadook', 'boogey man', 'silly sausage']
+
 
 @app.route("/")
 def start_here():
@@ -23,7 +25,7 @@ def start_here():
     <html>
       <body>
         Hi! This is the home page. 
-        <a href="/hello">Link Text</a>
+        <a href="/hello">Go to Hello!</a>
       </body>
     </html>
     """
@@ -33,7 +35,22 @@ def start_here():
 def say_hello():
     """Say hello and prompt for user's name."""
 
-    return """
+    option_string = ""
+    for item in AWESOMENESS:
+      print(item)
+
+      # print("""
+      # <option value ="{}">{}</option>
+      # """.format(item, item))
+
+
+      option_string += """
+      <option value ="{}">{}</option>
+      """.format(item, item)
+
+      print('option string:', option_string)
+
+    html_string = """
     <!doctype html>
     <html>
       <head>
@@ -43,11 +60,50 @@ def say_hello():
         <h1>Hi There!</h1>
         <form action="/greet">
           What's your name? <input type="text" name="person">
+          <select name="awesomeness">
+    """ + option_string + """
+          </select> <br> <br>
           <input type="submit" value="Submit">
         </form>
+        <a href="/diss">Go to disses!</a>
       </body>
     </html>
     """
+
+    return html_string
+
+    # return """
+    # <!doctype html>
+    # <html>
+    #   <head>
+    #     <title>Hi There!</title>
+    #   </head>
+    #   <body>
+    #     <h1>Hi There!</h1>
+    #     <form action="/greet">
+    #       What's your name? <input type="text" name="person">
+    #       <select name="awesomeness">
+    #         <option value="awesome">Awesome</option>
+    #         <option value="terrific">Terrific</option>
+    #         <option value="fantastic">Fantastic</option>
+    #         <option value="neato">Neato</option>
+    #         <option value="fantabulous">fantabulous</option>
+    #         <option value="wowza">Wowza</option>
+    #         <option value="oh-so-not-meh">oh-so-not-meh</option>
+    #         <option value="brilliant">Brilliant</option>
+    #         <option value="ducky">Ducky</option>
+    #         <option value="coolio">Coolio</option>
+    #         <option value="incredible">Incredible</option>
+    #         <option value="wonderful">Wonderful</option>
+    #         <option value="smashing">Smashing</option>
+    #         <option value="lovely">Lovely</option> 
+    #       </select> <br> <br>
+    #       <input type="submit" value="Submit">
+    #     </form>
+    #     <a href="/diss">Go to disses!</a>
+    #   </body>
+    # </html>
+    # """
 
 
 @app.route("/greet")
@@ -55,10 +111,15 @@ def greet_person():
     """Get user by name."""
 
     player = request.args.get("person")
+    # compliment = request.args.get("awesomeness")
+    attribute = request.args.get("awesomeness")
 
-    compliment = choice(AWESOMENESS)
+    if attribute == None:
+        attribute = request.args.get("disses")
 
-    y = x
+    # compliment = choice(AWESOMENESS)
+
+    # y = x
 
     return """
     <!doctype html>
@@ -70,7 +131,37 @@ def greet_person():
         Hi, {}! I think you're {}!
       </body>
     </html>
-    """.format(player, compliment)
+    """.format(player, attribute)
+
+@app.route("/diss")
+def diss_person():
+
+      return """
+      <!doctype html>
+      <html>
+        <head>
+          <title>Hi There!</title>
+        </head>
+        <body>
+          <h1>Hi There!</h1>
+          <form action="/greet">
+            What's your name? <input type="text" name="person">
+            <select name="disses">
+              <option value="dummkopf">Dummkopf</option>
+              <option value="rumpelstilzchen">Rumpelstilzchen</option>
+              <option value="rumpus">Rumpus</option>
+              <option value="babadook">Babadook</option>
+              <option value="boogey man">Boogey Man</option>
+              <option value="silly sausage">Silly Sausage</option>
+            </select> <br> <br>
+            <input type="submit" value="Submit">
+          </form>
+          <a href="/diss">Go to disses!</a>
+        </body>
+      </html>
+      """
+
+
 
 
 if __name__ == "__main__":
